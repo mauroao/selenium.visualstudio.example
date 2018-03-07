1583bfa525a4eb438829575c96a447f37b2243ab
INTRA\mauro.oliveira <mauro.oliveira@agilecontent.com>
2018-03-07 15:42:52 -0300

    First Commit

---
+++ WebDriver_basics/CheckSauceLabsHomePage.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace WebDriver_basics
+{
+    [TestFixture]
+    public class CheckSauceLabsHomePage
+    {
+
+        [Test]
+        public void site_header_is_on_home_page()
+        {
+            IWebDriver browser = new ChromeDriver();
+            //Firefox's proxy driver executable is in a folder already
+            //  on the host system's PATH environment variable.
+            browser.Navigate().GoToUrl("http://saucelabs.com");
+            IWebElement header = browser.FindElement(By.Id("site-header"));
+    
+            Assert.True(header.Displayed);
+            browser.Close();
+        }
+    }
+}
